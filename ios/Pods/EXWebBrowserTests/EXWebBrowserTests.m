d8b42376e71ab1cc1b071ad902ca590833ee19f6
Michał Czernek <czernekmichal@gmail.com>
2020-03-10 11:10:54 +0100

    Add tests to EXWebBrowser.

---
+++ ios/Pods/EXWebBrowserTests/EXWebBrowserTests.m
@@ -0,0 +1,37 @@
+//
+//  EXWebBrowserTests.m
+//  EXWebBrowserTests
+//
+//  Created by Michał Czernek on 09/03/2020.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface EXWebBrowserTests : XCTestCase
+
+@end
+
+@implementation EXWebBrowserTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+  XCTAssert(true);
+}
+
+//- (void)testPerformanceExample {
+//    // This is an example of a performance test case.
+//    [self measureBlock:^{
+//        // Put the code you want to measure the time of here.
+//    }];
+//}
+
+@end
---
+++ packages/expo-web-browser/ios/EXWebBrowserTests/EXWebBrowserTests.m
@@ -0,0 +1,17 @@
+#import <XCTest/XCTest.h>
+#import "EXWebBrowser.h"
+
+@interface EXWebBrowserTests : XCTestCase
+
+@end
+
+@implementation EXWebBrowserTests
+
+#pragma mark - emptyTest
+
+- (void)testEmpty
+{
+  XCTAssert(true);
+}
+
+@end
